01f2cbfb82ce8cf633aab15cc387924886bf6b9c
sanjit <sanjeet291196@gmail.com>
2016-12-25 16:10:21 +0530

    Chat and Login

--- app/src/main/java/com/bookmeds/youngagain/DailyRemainderActivity.java
+++ app/src/main/java/com/bookmeds/youngagain/DailyRemainderActivity.java
@@ -1,7 +1,10 @@
 package com.bookmeds.youngagain;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import java.util.ArrayList;
@@ -13,7 +16,26 @@ public class DailyRemainderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_remainder);
-        menuList = (GridView) findViewById(R.id.menulist);
-        ArrayList<MenuItem> items = new ArrayList<>();
+
+        menuList = (GridView) findViewById(R.id.remainder_list);
+
+        final ArrayList<MenuItem> items = new ArrayList<>();
+        items.add(new MenuItem(R.drawable.daily_remainders, "Ideal Diet Chart", ""));
+        items.add(new MenuItem(R.drawable.daily_remainders, "Timetable", ""));
+        final menuAdapter adapter = new menuAdapter(this, items);
+        menuList.setAdapter(adapter);
+        menuList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        startActivity(new Intent(DailyRemainderActivity.this, DietActivity.class));
+                        break;
+                    case 1:
+                        startActivity(new Intent(DailyRemainderActivity.this, TimeTableActivity.class));
+                        break;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/bookmeds/youngagain/MedicineListActivity.java
+++ app/src/main/java/com/bookmeds/youngagain/MedicineListActivity.java
@@ -77,5 +77,5 @@ public class MedicineListActivity extends AppCompatActivity {
         });
     }
 
-    ;
+
 }
